0386e255cc6699039e4c2771bb35310212c37ee9
Duckusss <hoangducng165@gmail.com>
2026-08-19 17:09:49 +0700

    Refactor binary addition logic for clarity

--- 67. Add Binary.py
+++ 67. Add Binary.py
@@ -5,12 +5,18 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        ans = int(a) + int(b)
-        for i in range(len(str(ans))):
-            if int(str(ans)[-1*(i+1)]) >= 2:
-                ans += 10**(i+1)
-                ans -= 2*(10**i)
-        return str(ans)
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        ans = ""
+        while (i >= 0 or j >= 0):
+            k = int(a[i]) if i >= 0 else 0
+            l = int(b[j]) if j >= 0 else 0
+            c = k + l + carry
+            carry = c // 2
+            ans = str(c % 2) + ans
+            [i, j] = [i - 1, j - 1]
+        return "1" + ans if carry else ans
 
         # format(int(a,2)+int(b,2), "b")
         
