937a626691933e85ec1adc341bd9e01633108eb6
Duckusss <hoangducng165@gmail.com>
2026-08-25 17:26:58 +0700

    Replace old sqrt implementation with binary search
    Refactor square root calculation to use binary search.

--- 69. Sqrt(x).py
+++ 69. Sqrt(x).py
@@ -4,20 +4,30 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        i = 1
-        if x < 2:
-            return x
-        while i * i <= x:
-            i *= 2
-        i /= 2
-        if i * i == x:
-            return i
-        while i * i <= x:
-            i += 1000
-        while i * i > x:
-            i -= 100
-        while i * i <= x:
-            i += 10
-        while i * i > x:
-            i -= 1
-        return i
+        # i = 1
+        # if x < 2:
+        #     return x
+        # while i * i <= x:
+        #     i *= 2
+        # i /= 2
+        # if i * i == x:
+        #     return i
+        # while i * i <= x:
+        #     i += 1000
+        # while i * i > x:
+        #     i -= 100
+        # while i * i <= x:
+        #     i += 10
+        # while i * i > x:
+        #     i -= 1
+        # return i
+        h = x
+        l = 0
+        c = 1
+        while h-l > 1:
+            c = l+(h-l+1)//2
+            if c*c > x:
+                h = c
+            else:
+                l = c
+        return l if x > 1 else x
