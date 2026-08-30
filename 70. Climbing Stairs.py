1d55a149ab2e41e0282593ef0291578506c1166d
Duckusss <hoangducng165@gmail.com>
2026-08-30 22:10:20 +0700

    Implement climbing stairs solution in Python

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ways = [1] * (n+1)
+        for i in range(2,(n+1)):
+            ways[i] = ways[i-1] + ways[i-2]
+        return ways[-1]
+        
